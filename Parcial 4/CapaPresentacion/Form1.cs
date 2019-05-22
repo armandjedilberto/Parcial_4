2f2e93d8138dd9fbcaabc53935f1f18bec4bfeed
armandjedilberto <47874640+armandjedilberto@users.noreply.github.com>
2019-05-21 20:03:11 -0600

    Add files via upload

---
+++ Parcial 4/CapaPresentacion/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using capaNegocio;
+namespace CapaPresentacion
+{
+    public partial class Form1 : Form
+    {
+        Consultas neg = new Consultas();
+        
+        Consultas enlc = new Consultas();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(enlc.prueba());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            neg.CodPlantilla = textBox1.Text;
+            neg.NombreVendor = textBox2.Text;
+            neg.insertPlantilla();
+            limpiar();
+            cargar();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            neg.CodPlantilla = textBox1.Text;
+            neg.NombrePlantilla = textBox2.Text;
+            neg.updatePlantilla();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            neg.CodPlantilla = textBox1.Text;
+            neg.deletPlantilla();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        public void limpiar() {
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+        }
+        public void cargar() {
+            neg.mostrarPlantilla();
+            dataGridView1.DataSource = neg.dt;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            limpiar();
+            cargar();
+        }
+    }
+}
---
+++ Parcial 4/capaNegocio/Consultas.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;//*****-
+namespace capaNegocio
+{
+    public class Consultas
+    {
+        bdConex test= new bdConex();
+        private String codVendor, nombrePlantilla;
+        public DataTable dt = new DataTable();//**
+        public String NombreVendor
+        {
+            get { return nombrePlantilla; }
+            set { nombrePlantilla = value; }
+        }
+
+        public String CodPlantilla
+        {
+            get { return codPlantilla; }
+            set { codPlantilla = value; }
+        }
+        //
+
+        public void insertPlantilla()
+        {
+            try {
+            String insert="INSERT INTO PLANTILLA(Plantilla,NOMBRE)VALUES('"+codPlantilla+"','"+nombrePlantilla+"')";
+                    test.Conectar();
+                    test.consultaSQL(insert);
+                    test.Desconectar();         
+            }
+            catch {
+            
+            }
+        }
+
+
+        public void updatePlantilla()
+        {
+            try
+            {
+                String update = "update Plantilla set NOMBRE='"
+                                 + nombrePlantilla + "' where Plantilla='"
+                                 +codPlantilla + "'";
+                test.Conectar();
+                test.consultaSQL(update);
+                test.Desconectar();
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void deletePlantilla()
+        {
+            try
+            {
+                String delete = "delete from Plantilla where plantilla='"
+                                 + codPlantilla + "'";
+                test.Conectar();
+                test.consultaSQL(delete);
+                test.Desconectar();
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        public String prueba()
+        {
+            String mensaje;
+            if (test.Conectar() == true)
+            {
+                test.Desconectar();
+                mensaje = "Conexion realizada";
+            }
+            else {
+                test.Desconectar();
+                mensaje = "Conexion no realizada";
+            }
+            return mensaje;
+
+        }
+
+        public void mostrarPlantilla()
+        {
+            try
+            {
+                String consulta = "select Plantilla as CODPlantilla,NOMBRE as NomPlatilla from [dbo].[Plantilla]";
+                test.Conectar();
+                test.mostrarSQL(consulta);
+                test.da.Fill(dt);
+                test.Desconectar();
+            }
+            catch
+            {
+
+            }
+        }
+
+
+
+
+        public object nombrêPlantilla { get; set; }
+
+        public string codPlantilla { get; set; }
+
+        public string nombrePlantilla { get; set; }
+    }
+}
